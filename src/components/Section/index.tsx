1b7f7b8dbf821180cf387a4994e0f542a561ade2
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2021-01-12 22:29:32 -0300

    componentsSection

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+interface Props {
+  variant: 'blue' | 'beige' | 'white' | 'black';
+  title: string;
+  description: string;
+}
+
+const Section: React.FC<Props> = () => {
+  return <div />;
+};
+
+export default Section;
