f28d3f6bb482078366d7e070094aa06c68486e85
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2021-01-12 22:44:07 -0300

    variantStyles

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -1,3 +1,44 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  --padding-top: 100px;
+  --padding-bottom: 120px;
+  --heading-font-size: 32px;
+  --content-width: 100%;
+
+  &.blue {
+    --bg-color: var(--color-primary);
+    --text-color: var(--color-tertiary);
+    --logo-color: var(--color-secondary);
+  }
+
+  &.beige {
+    --bg-color: var(--color-secondary);
+    --text-color: var(--color-quaternary);
+    --logo-color: var(--color-primary);
+  }
+
+  &.white {
+    --bg-color: var(--color-tertiary);
+    --text-color: var(--color-quaternary);
+    --logo-color: var(--color-blue);
+  }
+
+  &.white {
+    --bg-color: var(--color-quaternary);
+    --text-color: var(--color-tertiary);
+    --logo-color: var(--color-blue);
+  }
+
+  &:first-child {
+    --padding-top: 130px;
+    --heading-font-size: 51px;
+
+    @media (min-width: 1024) {
+      --content-width: 50%;
+      --heading-font-size: 71px;
+    }
+  }
+`;
+
+export const Content = styled.div``;
